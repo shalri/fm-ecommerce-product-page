2a68c9b06b637b9b25ee79bfc84a1d2b7e78055e
shalri <fsaonlinework777@gmail.com>
2024-07-02 21:16:49 +0800

    Cart items animation

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,6 +6,7 @@ import Cart from "./Cart";
 import { useState } from "react";
 import Nav from "./Nav";
 import { cn } from "@/lib/utils";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function Header() {
   const [showCart, setShowCart] = useState(false);
@@ -23,6 +24,12 @@ export default function Header() {
   };
   return (
     <>
+      <div
+        className={cn(
+          showMobileNav &&
+            "absolute z-[2] h-full w-full bg-black/10 backdrop-blur-md",
+        )}
+      />
       <Cart show={showCart} onClose={() => setShowCart(false)} />
       <header className="mb-0 px-6">
         <div className="flex w-full items-center justify-between pb-6 pt-4">
@@ -48,9 +55,19 @@ export default function Header() {
               onClick={handleToggleCart}
               className="relative h-5 w-6 bg-[url(/images/icon-cart.svg)] bg-center bg-no-repeat"
             >
-              <span className="absolute -right-[4px] -top-[6px] h-[14px] w-5 rounded-full bg-ep-orange text-[.65rem] font-bold text-white">
-                {itemCount}
-              </span>
+              <AnimatePresence>
+                {itemCount > 0 && (
+                  <motion.span
+                    key={itemCount === 0 ? "empty" : itemCount}
+                    initial={{ opacity: 0, scale: 0 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    exit={{ opacity: 0, scale: 0 }}
+                    className="absolute -right-[4px] -top-[6px] h-[14px] w-5 rounded-full bg-ep-orange text-[.65rem] font-bold text-white"
+                  >
+                    {itemCount}
+                  </motion.span>
+                )}
+              </AnimatePresence>
             </button>
             <div className="relative h-6 w-6 overflow-hidden rounded-full">
               <Image
