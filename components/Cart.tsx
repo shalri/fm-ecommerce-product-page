f93a785dc8d9a4c9fba2b485fc6ebfbdfaa73ae4
shalri <fsaonlinework777@gmail.com>
2024-07-02 14:36:37 +0800

    Cart active layout and animation

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -1,5 +1,6 @@
 import { useCart } from "@/contexts/CartContext";
 import { cn } from "@/lib/utils";
+import { AnimatePresence, motion } from "framer-motion";
 import Image from "next/image";
 import { useEffect, useRef } from "react";
 interface CartProps {
@@ -32,31 +33,59 @@ export default function Cart({ show, onClose }: CartProps) {
   };
 
   return (
-    <aside
-      ref={cartRef}
-      className={cn(
-        "absolute inset-0 bottom-0 left-4 right-4 top-16 z-20 block max-h-[40dvh] bg-white",
-        show ? "block" : "hidden",
+    <AnimatePresence>
+      {show && (
+        <motion.aside
+          key="cart"
+          initial={{ opacity: 0, y: -100 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: 10 }}
+          ref={cartRef}
+          className={cn(
+            "absolute inset-0 bottom-0 left-2 right-2 top-[76px] z-20 block max-h-[256px] rounded-[8px] bg-white py-4",
+          )}
+        >
+          <h2 className="border-b border-ep-light-grayish-blue px-6 pb-6 pt-1 font-bold">
+            Cart
+          </h2>
+          {itemCount === 0 ? (
+            <div className="flex items-center justify-center px-6 pt-6">
+              <p className="mt-10 font-bold text-ep-dark-grayish-blue">
+                Your cart is empty.
+              </p>
+            </div>
+          ) : (
+            <div className="px-6 pt-6">
+              <div className="flex items-center justify-between">
+                <Image
+                  src={itemThumbnail}
+                  alt={`${itemName} thumbnail`}
+                  width={50}
+                  height={50}
+                  className="rounded-md"
+                />
+                <div className="flex flex-col justify-between">
+                  <h3 className="text-ep-dark-grayish-blue">{itemName}</h3>
+                  <p className="text-ep-dark-grayish-blue">
+                    ${itemPrice}&nbsp;&nbsp;x&nbsp;&nbsp;{itemCount}&nbsp;&nbsp;
+                    <strong className="text-ep-black">${total}</strong>
+                  </p>
+                </div>
+                <button
+                  onClick={handleClearCart}
+                  className="h-4 w-4 bg-[url(/images/icon-delete.svg)] bg-center bg-no-repeat"
+                ></button>
+              </div>
+              <a
+                className="transition-color mt-6 block w-full rounded-[8px] bg-ep-orange px-6 py-4 text-center font-bold duration-300 hover:bg-ep-orange/80"
+                href="#"
+              >
+                Checkout
+              </a>
+            </div>
+          )}
+        </motion.aside>
       )}
-    >
-      <h2 className="">Cart</h2>
-      {itemCount === 0 ? (
-        <p>Your cart is empty</p>
-      ) : (
-        <>
-          <h3 className="">{itemName}</h3>
-          <Image
-            src={itemThumbnail}
-            alt={`${itemName} thumbnail`}
-            width={50}
-            height={50}
-          />
-          <p className="">
-            ${itemPrice} + {itemCount} <strong>{total}</strong>
-          </p>
-          <button onClick={handleClearCart}>Clear Cart</button>
-        </>
-      )}
-    </aside>
+    </AnimatePresence>
   );
 }
